884207105e98fcaafea4b409707e99005df78f44
Gunnar Magholder <gmagholder@gmail.com>
2018-07-05 07:01:41 +0200

    Cleaning up the css for table rows

--- src/components/FactBox.js
+++ src/components/FactBox.js
@@ -18,15 +18,15 @@ class FactBox extends Component {
       <table>
         <tbody>
           <tr>
-              <td className="car_containers">Container: </td>
+              <td className="car_containers car_row">Container: </td>
               <td className="car_number">{this.state.containers}</td>
           </tr>
           <tr>
-              <td className="car_trains">Trains: </td>
+              <td className="car_trains car_row">Trains: </td>
               <td className="car_number">{this.state.trains}</td>
           </tr>
           <tr>
-              <td className="car_currentships">Ships:   </td>
+              <td className="car_currentships car_row">Ships:   </td>
               <td className="car_number">{this.state.currentShips}</td>
           </tr>
           </tbody>
