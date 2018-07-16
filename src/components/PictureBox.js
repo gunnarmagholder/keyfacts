a20ee581ac80dec584361e9623b6732abb612c57
Gunnar Magholder <gmagholder@gmail.com>
2018-07-16 17:34:10 +0200

    Working on the Twitter reader

--- src/components/PictureBox.js
+++ src/components/PictureBox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Twitter from 'twitter';
 import '../Caroussel.css';
 
 
@@ -7,16 +7,30 @@ class PictureBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        pictureUrl: './assets/ship.jpg'
+        pictureUrl: this.getImageUrl()
     }
   }
   render() {
     return (
      <div className="car_picturebox">
-        <img alt="Ship of the week" src={this.state.pictureUrl} width={300} height={250} mode='fit' />
+        <img alt="Ship of the week" src={this.state.pictureUrl} width={250} height={200} mode='fit' />
     </div>
     );
   }
+  getImageUrl() {
+    var image_url = '';
+    var client = new Twitter({
+      consumer_key: 'aIgLWM46xW4KbFK62j2nL0Zlq',
+      consumer_secret: 'SzM4qv4Wuk3U9PTrOzG2ULG8KuqNhlzZw53qGET7pdNASTdrL3',
+      access_token_key: '15296186-3R0NtUMfylTTnUHeyXZqsK3rlKnaFAIUF4eb06Vi4',
+      access_token_secret: 'rOvE6ABRLcTshgMKtQ4KneYFxekSkXuxE63Eo1MCD7c0o'
+    });
+    client.get('search/tweets', {q: '#schiffdestages @portofhamburg', tweet_mode: 'extended'}, function(error, tweets, response) {
+      image_url = tweets['statuses'][0]['extended_entities']['media'][0]['media_url']
+    });
+    return image_url;
+  }
+
 }
 
 export default PictureBox;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import Caroussel from './containers/Caroussel';
 
 function tick() {
   var current = new Date();
-  var minutes = current.getMinutes();
-  if ((minutes%2)===0) {
-      ReactDOM.render(<Caroussel tag='facts' />, document.getElementById('root'));
-  } else {
+  var seconds = current.getSeconds();
+  //if ((seconds>0 && seconds < 30)) {
+  //    ReactDOM.render(<Caroussel tag='facts' />, document.getElementById('root'));
+  //} else {
       ReactDOM.render(<Caroussel tag='picture' />, document.getElementById('root'));
-  }
+  //}
 }
 
 setInterval(tick, 1000);
