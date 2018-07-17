7620d1aead65601de2107db15a08fb0b7821bec1
Gunnar Magholder <gmagholder@gmail.com>
2018-07-17 15:14:11 +0200

    Working on a node script to fetch the ship of the day

--- src/components/PictureBox.js
+++ src/components/PictureBox.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Twitter from 'twitter';
 import '../Caroussel.css';
 
 
@@ -7,8 +6,7 @@ class PictureBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        pictureUrl: 'https://pbs.twimg.com/media/DiNq-_7W0AAOEQu.jpg:large',
-	configData: require('../config.development.json')
+        pictureUrl: '/assets/ship.jpg'
     }
   }
   render() {
@@ -18,15 +16,6 @@ class PictureBox extends Component {
     </div>
     );
   }
-  getImageUrl() {
-    var image_url = '';
-    var client = new Twitter(this.state.configData);
-    client.get('search/tweets', {q: '#schiffdestages @portofhamburg', tweet_mode: 'extended'}, function(error, tweets, response) {
-      image_url = tweets['statuses'][0]['extended_entities']['media'][0]['media_url']
-    });
-    return image_url;
-  }
-
 }
 
 export default PictureBox;
--- src/downloadsod.js
+++ src/downloadsod.js
@@ -1,2 +1,32 @@
-import Twitter from 'twitter';
+'use strict'
+
+var Twitter = require('twitter');
+var fs = require('fs');
+var http = require('http');
+var configData = require('./config.development.json');
+
+
+var client = new Twitter(configData);
+var image_url = '';
+client.get('search/tweets', {q: '#schiffdestages from:portofhamburg', tweet_mode: 'extended'}, function(error, tweets, response) {
+  if(error) {
+  	console.log(error);
+  }
+  console.log(tweets['statuses'][0]);
+  image_url = tweets['statuses'][0]['extended_entities']['media'][0]['media_url']
+  console.log(image_url);
+  var file = fs.createWriteStream('../public/assets/ship.jpg');
+  var request = http.get(image_url, (response) => {
+    response.pipe(file);
+    file.on('finish', () => { 
+      file.close();
+    });
+  }).on('error', (err) => {
+    if(err) { 
+      fs.unlink(dest); 
+      console.log(err);
+    };
+  });
+
+});
 
