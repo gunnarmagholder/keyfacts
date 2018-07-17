92b3f980bcad7f0efd44e559023a210a20e62f4a
Gunnar Magholder <gmagholder@gmail.com>
2018-07-17 06:20:33 +0200

    Loading configuration from a json file

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import Caroussel from './containers/Caroussel';
 function tick() {
   var current = new Date();
   var seconds = current.getSeconds();
-  //if ((seconds>0 && seconds < 30)) {
-  //    ReactDOM.render(<Caroussel tag='facts' />, document.getElementById('root'));
-  //} else {
-      ReactDOM.render(<Caroussel tag='picture' />, document.getElementById('root'));
-  //}
+  if(seconds > 0 && seconds < 60) {
+	ReactDOM.render(<Caroussel tag='picture' />, document.getElementById('root'));
+  }
 }
 
 setInterval(tick, 1000);
