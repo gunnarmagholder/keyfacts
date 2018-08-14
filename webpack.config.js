ea0e7a351baa061ab05795373b258e0190a84b5e
Gunnar Magholder <gmagholder@gmail.com>
2018-08-14 08:59:08 +0200

    Timebased switching between contents

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //...
+    output: {
+      path: path.resolve(__dirname, 'public/assets'),
+      publicPath: 'http://dobby.intranet.hpa/keyfacts/assets',
+      path: path.resolve(__dirname/, 'static'),
+      publicPath: 'http://dobby.intranet.hoa/keyfacts/static',
+    }
+  };
