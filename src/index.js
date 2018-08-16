2ab8f07facc2f129b914108b60d1777f033a0ecd
Gunnar Magholder <gmagholder@gmail.com>
2018-08-16 14:37:33 +0200

    Get rid of the timer in index.js - timing is done in the caroussel part now

--- src/index.js
+++ src/index.js
@@ -5,14 +5,4 @@ import Caroussel from './containers/Caroussel';
 
 
 
-function tick() {
-  var current = new Date();
-  var seconds = current.getSeconds();
-  if(seconds > 0 && seconds < 30) {
-	  ReactDOM.render(<Caroussel tag='picture' />, document.getElementById('root'));
-  } else {
-    ReactDOM.render(<Caroussel tag='facts' />, document.getElementById('root'));
-  }
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(<Caroussel />, document.getElementById('root'));
