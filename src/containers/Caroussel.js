7ceb2bb332222b28e8239c687b0528c827b50902
Gunnar Magholder <gmagholder@gmail.com>
2018-11-05 09:15:56 +0100

    Prettying up source code and changed to eslint - because React

--- src/containers/Caroussel.js
+++ src/containers/Caroussel.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import FactBox from '../components/FactBox';
 import PictureBox from '../components/PictureBox';
-import TrafficInfo from '../components/TrafficInfo';
+// import TrafficInfo from '../components/TrafficInfo';
 
 class Caroussel extends Component {
-  config = { 
-    "timeBetweenComponents": 30000,
+  config() { 
+    return {"timeBetweenComponents": 30000};
   }
-  components = [
-    PictureBox,
-    FactBox,
-    // TrafficInfo,
-  ];
+  components() { 
+    return [
+      PictureBox,
+      FactBox,
+      // TrafficInfo,
+    ]
+  }
   constructor(props) {
     super(props);
     this.state = {
       componentToRender: 0
     }
-  };
+  }
   componentDidMount() {
     setInterval(() => {
       var newComponentToRender = this.state.componentToRender < this.components.length-1 ? this.state.componentToRender+1 : 0; 
