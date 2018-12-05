39e020047d5fbd57e10aac5b086561a075017b59
Gunnar Magholder <gmagholder@gmail.com>
2018-12-05 15:12:26 +0100

    Timer is foolin around

--- src/containers/Caroussel.js
+++ src/containers/Caroussel.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import FactBox from '../components/FactBox';
 import PictureBox from '../components/PictureBox';
+import { throws } from 'assert';
 // import TrafficInfo from '../components/TrafficInfo';
 
 class Caroussel extends Component {
   config() { 
-    return {"timeBetweenComponents": 1000};
+    return {"timeBetweenComponents": 10000};
   }
   components() { 
     return [
@@ -22,14 +23,20 @@ class Caroussel extends Component {
   }
   componentWillMount() {
     setInterval(() => {
-      
-      var newComponentToRender = this.state.componentToRender < this.components.length-1 ? this.state.componentToRender+1 : 0; 
+      console.log("Hit interval");
+      var thisMoment = new Date(Date.now());
+      // var thisMinute = thisMoment.getMinutes();
+      var thisMinute=31;
+      var lengthOfInterval = 60/this.components.length;
+      var newComponentToRender = Math.floor(thisMinute/lengthOfInterval);
+      // var newComponentToRender = this.state.componentToRender < this.components.length-1 ? this.state.componentToRender+1 : 0; 
+      // console.log("rendering item # " + newComponentToRender);
       this.setState({ componentToRender: newComponentToRender });
     },this.config.timeBetweenComponents);
   }
   render() {
     const TagName = this.components()[this.state.componentToRender];
-    console.log("Rendering " + TagName);
+    
     return (
       <div className="car_box">
         <TagName />
